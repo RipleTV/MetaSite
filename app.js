310bb15b170b8bcd7ae8bf52fda67a2e2ff05b5a
RipleTV <gryshyn2000@gmail.com>
2022-01-21 08:44:29 +0200

    app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 // function  wrok when you press button "Click"
 async function loginWithEth() {
-    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-    // Prompt user for account connections
-    await provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner();
-    // show in console your address. (you can comment console.log)
-    console.log("Account:", await signer.getAddress());
+    if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+        // Prompt user for account connections
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        // show in console your address. (you can comment console.log)
+        console.log("Account:", await signer.getAddress());
 
-    // edit text from "Address" to "0x123..."
-    document.querySelector('.walletAddress').innerHTML = await signer.getAddress();
+        // edit text from "Address" to "0x123..."
+        document.querySelector('.walletAddress').innerHTML = await signer.getAddress();
+    } else {
+        alert('You must install MetaMask')
+    }
 }
